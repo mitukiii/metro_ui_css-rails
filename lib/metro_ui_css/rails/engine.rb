cf69c4dfab2af244f5fcbbd93d40ccf0a58344df
Kazuya Takeshima <mail@mitukiii.jp>
2014-09-19 03:27:51 +0900

    Add fonts to assets precompile

--- lib/metro_ui_css/rails/engine.rb
+++ lib/metro_ui_css/rails/engine.rb
@@ -1,6 +1,9 @@
 module MetroUiCss
   module Rails
     class Engine < ::Rails::Engine
+      initializer 'metro_ui_css-rails.setup' do |app|
+        app.config.assets.precompile << %r(metro/.+\.(:?ttf|woff|svg))
+      end
     end
   end
 end
