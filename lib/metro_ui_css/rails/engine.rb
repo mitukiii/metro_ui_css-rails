79daf81b917fc9af3a6038c75864fe1e9c0f5f98
Kazuya Takeshima <mail@mitukiii.jp>
2014-09-19 03:32:40 +0900

    nyan

--- lib/metro_ui_css/rails/engine.rb
+++ lib/metro_ui_css/rails/engine.rb
@@ -2,7 +2,7 @@ module MetroUiCss
   module Rails
     class Engine < ::Rails::Engine
       initializer 'metro_ui_css-rails.setup' do |app|
-        app.config.assets.precompile << %r(metro/.+\.(:?ttf|woff|svg))
+        app.config.assets.precompile << %r(\Ametro/.+\.(:?ttf|woff|svg)\Z)
       end
     end
   end
